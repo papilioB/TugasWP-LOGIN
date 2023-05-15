45f6bd9f039c2da960356f7872fc8ffffa53d301
papilioB <papilio.aegeus@gmail.com>
2023-05-15 19:32:07 +0700

    tugaswpQ

---
+++ application/views/v_about.php
@@ -0,0 +1,28 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Bagas </li>
+        <li>Nama Belakang : Bhamakerti</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Jl. J Buntu, Kebon Baru </li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>Jakarta Selatan</li>
+</ul> 
+
+<h4>Tanggal Lahir</h4>
+<ul type="none">
+    <li>17 Oktober 2000</li>
+</ul>
+ <h4>Hobi</h4>
+ <ul type="square">
+    <li>Design</li>
+    <li>Menggambar</li>
+</ul>
+</section>
